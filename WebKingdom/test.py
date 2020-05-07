ae681cd4e55451e916d47f7ccbf76680c8600407
alexhaislip <haislipalexander@gmail.com>
2020-05-06 23:44:56 -0500

    just making some cool changes to folder names agian

---
+++ WebKingdom/test.py
@@ -0,0 +1,37 @@
+# SOMA's CODE!
+<<<<<<< HEAD
+# Python program to display the Fibonacci sequence
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+=======
+
+# Python program to display the Fibonacci sequence
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+>>>>>>> 972b4d2517f86efe4c4015a06f5ddc23bbf171a0
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
+
+<<<<<<< HEAD
+=======
+
+>>>>>>> 972b4d2517f86efe4c4015a06f5ddc23bbf171a0
